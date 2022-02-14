3965d6e26bf5f6792fc0285e99632935a9093429
Ravinder Nath <ravi89nath@gmail.com>
2022-02-14 23:27:42 +0530

    Update ECommerceDB-EXPFile.sql

--- ECommerceDB-EXPFile.sql
+++ ECommerceDB-EXPFile.sql
@@ -172,4 +172,20 @@ from customer
 where cus_name like 'A%' or cus_name like '%A';
 
 
+
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `group_suppliers`()
+BEGIN
+	select supplier.supp_id, supplier.supp_name, rating.rat_ratstars,
+    case
+		when rating.rat_ratstars > 4 then 'Genuine Supplier'
+        when rating.rat_ratstars > 2 then 'Average Supplier'
+        else 'Supplier should not be Considered'
+	end as verdict
+    from rating
+    inner join supplier on supplier.supp_id=rating.supp_id
+    order by rating.rat_ratstars desc;
+END
+
 call group_suppliers();
