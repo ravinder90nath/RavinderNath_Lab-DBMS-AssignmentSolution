ffc15f32f2982e65caf8a2369776dfd07c15e78f
Ravinder Nath <ravi89nath@gmail.com>
2022-02-14 23:26:59 +0530

    Add files via upload
    First Commit
    EcommerceDB-EXPFile.sql ( Save query file)
    EcommerceDB.sql (Export File)

---
+++ ECommerceDB-EXPFile.sql
@@ -0,0 +1,175 @@
+create database ecommerceDB;
+use ecommerceDB;
+
+drop table Supplier;
+drop table Customer;
+drop table Category;
+drop table Product;
+drop table ProductDetails;
+drop table Orders;
+drop table Rating;
+
+create table supplier(
+SUPP_ID int,
+SUPP_NAME varchar(50),
+SUPP_CITY varchar(50),
+SUPP_PHONE bigint
+);
+
+create table Customer(
+CUS_ID int,
+CUS_NAME varchar(50),
+CUS_PHONE bigint,
+CUS_CITY varchar(50),
+CUS_GENDER varchar(10)
+);
+
+create table Category(
+CAT_ID int,
+CAT_NAME varchar(50)
+);
+
+create table Product(
+PRO_ID int,
+PRO_NAME varchar(50),
+PRO_DESC varchar(50),
+CAT_ID int
+);
+
+create table ProductDetails(
+PROD_ID int,
+PRO_ID int,
+SUPP_ID int,
+PRICE bigint
+);
+
+create table Orders(
+ORD_ID int,
+ORD_AMOUNT bigint,
+ORD_DATE varchar(50),
+CUS_ID int,
+PROD_ID int
+);
+
+create table Rating(
+RAT_ID int,
+CUS_ID int,
+SUPP_ID int,
+RAT_RATSTARS int
+);
+
+insert into Supplier (SUPP_ID, SUPP_NAME, SUPP_CITY, SUPP_PHONE)
+values
+(1, 'Rajesh Retails', 'Delhi', 1234567890),
+(2, 'Appario Ltd.', 'Mumbai', 2589631470),
+(3, 'Knome products', 'Banglore',	9785462315),
+(4, 'Bansal Retails', 'Kochi', 8975463285),
+(5, 'Mittal Ltd.', 'Lucknow', 7898456532)
+;
+
+insert into Customer (CUS_ID, CUS_NAME, CUS_PHONE, CUS_CITY, CUS_GENDER)
+values
+(1, 'AAKASH', 9999999999, 'DELHI', 'M'),
+(2, 'AMAN', 9785463215,	'NOIDA', 'M'),
+(3, 'NEHA', 9999999999,	'MUMBAI', 'F'),
+(4, 'MEGHA' , 9994562399, 'KOLKATA', 'F'),
+(5, 'PULKIT', 7895999999, 'LUCKNOW', 'M')
+;
+insert into Category (CAT_ID, CAT_NAME)
+values
+(1, 'BOOKS'),
+(2, 'GAMES'),
+(3, 'GROCERIES'),
+(4, 'ELECTRONICS'),
+(5, 'CLOTHES')
+;
+
+insert into Product (PRO_ID, PRO_NAME, PRO_DESC, CAT_ID)
+values
+(1, 'GTA V', 'DFJDJFDJFDJFDJFJF', 2),
+(2, 'TSHIRT', 'DFDFJDFJDKFD', 5),
+(3, 'ROG LAPTOP', 'DFNTTNTNTERND', 4),
+(4, 'OATS', 'REURENTBTOTH', 3),
+(5, 'HARRY POTTER', 'NBEMCTHTJTH', 1)
+;
+
+insert into ProductDetails (PROD_ID, PRO_ID, SUPP_ID, PRICE)
+values
+(1,	1, 2, 1500),
+(2,	3, 5, 30000),
+(3,	5, 1, 3000),
+(4,	2, 3, 2500),
+(5,	4, 1, 1000)
+;
+
+insert into Orders (ORD_ID, ORD_AMOUNT, ORD_DATE, CUS_ID, PROD_ID)
+values
+(20, 1500,  '2021-10-12', 3, 5),
+(25, 30500, '2021-09-16', 5, 2),
+(26, 2000,  '2021-10-05', 1, 1),
+(30, 3500,  '2021-08-16', 4, 3),
+(50, 2000,  '2021-10-06', 2, 1)
+;
+
+insert into Rating (RAT_ID, CUS_ID, SUPP_ID, RAT_RATSTARS)
+values
+(1, 2, 2, 4),
+(2, 3, 4, 3),
+(3, 5, 1, 5),
+(4, 1, 3, 2),
+(5, 4, 5, 4)
+;
+
+
+select * from Supplier;
+select * from Customer;
+select * from Category;	
+select * from Product;
+select * from ProductDetails;
+select * from Orders;	
+select * from Rating;
+
+
+
+
+select cus_gender, count(c.cus_id) 
+from customer c 
+inner join orders o on o.cus_id = c.cus_id 
+where ORD_AMOUNT >= 3000 group by cus_gender;
+
+
+select o.ord_id, pr.pro_name, o.ord_amount, o.ord_date, o.cus_id, pd.prod_id 
+from orders o inner join ProductDetails pd on pd.prod_id = o.prod_id 
+inner join product pr on pr.pro_id = pd.pro_id
+where o.cus_id = 2;
+
+
+select s.supp_id as 'Supplier Id', s.supp_name as 'Supplier Name', s.supp_city as 'City', s.supp_phone as 'Phone', count(pd.prod_id) as 'No of Products'
+from supplier s
+inner join ProductDetails pd on pd.supp_id = s.supp_id
+group by pd.supp_id
+having count(pd.prod_id) > 1
+order by count(pd.prod_id);
+
+
+select c.*, pr.pro_name, o.ord_amount
+from category c
+inner join product pr on pr.cat_id = c.cat_id
+inner join ProductDetails pd on pd.pro_id = pr.pro_id
+inner join orders o on o.prod_id = pd.prod_id
+having min(o.ord_amount);
+
+
+select o.ord_id as 'Order ID', pr.pro_id as 'Product ID', pr.pro_name as 'Product Name'
+from orders o
+inner join ProductDetails pd on pd.prod_id = o.prod_id
+inner join product pr on pr.pro_id = pd.pro_id
+where o.ord_date > '2021-10-05';
+
+
+select cus_name as 'Customer Name', cus_gender as 'Gender'
+from customer
+where cus_name like 'A%' or cus_name like '%A';
+
+
+call group_suppliers();
---
+++ ECommerceDB.sql
@@ -0,0 +1,202 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecommercedb
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `CAT_ID` int DEFAULT NULL,
+  `CAT_NAME` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'BOOKS'),(2,'GAMES'),(3,'GROCERIES'),(4,'ELECTRONICS'),(5,'CLOTHES');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `CUS_ID` int DEFAULT NULL,
+  `CUS_NAME` varchar(50) DEFAULT NULL,
+  `CUS_PHONE` bigint DEFAULT NULL,
+  `CUS_CITY` varchar(50) DEFAULT NULL,
+  `CUS_GENDER` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'AAKASH',9999999999,'DELHI','M'),(2,'AMAN',9785463215,'NOIDA','M'),(3,'NEHA',9999999999,'MUMBAI','F'),(4,'MEGHA',9994562399,'KOLKATA','F'),(5,'PULKIT',7895999999,'LUCKNOW','M');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `ORD_ID` int DEFAULT NULL,
+  `ORD_AMOUNT` bigint DEFAULT NULL,
+  `ORD_DATE` varchar(50) DEFAULT NULL,
+  `CUS_ID` int DEFAULT NULL,
+  `PROD_ID` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (20,1500,'2021-10-12',3,5),(25,30500,'2021-09-16',5,2),(26,2000,'2021-10-05',1,1),(30,3500,'2021-08-16',4,3),(50,2000,'2021-10-06',2,1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `PRO_ID` int DEFAULT NULL,
+  `PRO_NAME` varchar(50) DEFAULT NULL,
+  `PRO_DESC` varchar(50) DEFAULT NULL,
+  `CAT_ID` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'GTA V','DFJDJFDJFDJFDJFJF',2),(2,'TSHIRT','DFDFJDFJDKFD',5),(3,'ROG LAPTOP','DFNTTNTNTERND',4),(4,'OATS','REURENTBTOTH',3),(5,'HARRY POTTER','NBEMCTHTJTH',1);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productdetails`
+--
+
+DROP TABLE IF EXISTS `productdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productdetails` (
+  `PROD_ID` int DEFAULT NULL,
+  `PRO_ID` int DEFAULT NULL,
+  `SUPP_ID` int DEFAULT NULL,
+  `PRICE` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productdetails`
+--
+
+LOCK TABLES `productdetails` WRITE;
+/*!40000 ALTER TABLE `productdetails` DISABLE KEYS */;
+INSERT INTO `productdetails` VALUES (1,1,2,1500),(2,3,5,30000),(3,5,1,3000),(4,2,3,2500),(5,4,1,1000);
+/*!40000 ALTER TABLE `productdetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rating` (
+  `RAT_ID` int DEFAULT NULL,
+  `CUS_ID` int DEFAULT NULL,
+  `SUPP_ID` int DEFAULT NULL,
+  `RAT_RATSTARS` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+INSERT INTO `rating` VALUES (1,2,2,4),(2,3,4,3),(3,5,1,5),(4,1,3,2),(5,4,5,4);
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `SUPP_ID` int DEFAULT NULL,
+  `SUPP_NAME` varchar(50) DEFAULT NULL,
+  `SUPP_CITY` varchar(50) DEFAULT NULL,
+  `SUPP_PHONE` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+INSERT INTO `supplier` VALUES (1,'Rajesh Retails','Delhi',1234567890),(2,'Appario Ltd.','Mumbai',2589631470),(3,'Knome products','Banglore',9785462315),(4,'Bansal Retails','Kochi',8975463285),(5,'Mittal Ltd.','Lucknow',7898456532);
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-14 23:21:01
